19b6aac0df2ccdfca08f41ade0053bf01d06ed0c
Francisco Guilherme <forwork929@gmail.com>
2026-03-21 12:26:01 -0300

    Adicao de classe Anamnese

---
+++ src/main/java/br/ufca/sgco/model/Anamnese.java
@@ -0,0 +1,44 @@
+package br.ufca.sgco.model;
+
+public class Anamnese {
+    private String historico;
+    private String alergias;
+    private String observacoes;
+
+    public Anamnese() {}
+
+    public Anamnese(String historico, String alergias, String observacoes) {
+        this.historico = historico;
+        this.alergias = alergias;
+        this.observacoes = observacoes;
+    }
+
+    public String getHistorico() {
+        return historico;
+    }
+
+    public void setHistorico(String historico) {
+        this.historico = historico;
+    }
+
+    public String getAlergias() {
+        return alergias;
+    }
+
+    public void setAlergias(String alergias) {
+        this.alergias = alergias;
+    }
+
+    public String getObservacoes() {
+        return observacoes;
+    }
+
+    public void setObservacoes(String observacoes) {
+        this.observacoes = observacoes;
+    }
+
+    @Override
+    public String toString() {
+        return "Anamnese(Histórico: " + historico + ", Alergias: " + alergias + ", Observações: " + observacoes + ")";
+    }
+}
